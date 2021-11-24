f01ea171272fa744c4c82a65910c4d5b3299cc47
amir-malek <ahmalekpour1999@gmail.com>
2021-11-24 15:51:27 +0330

    redux added to project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import {Provider} from 'react-redux';
+import withRedux from 'next-redux-wrapper';
+import store from '../redux/store'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+    return <Provider store={store}>
+        <Component {...pageProps} />
+    </Provider>
 }
-
-export default MyApp
+const makeStore  = () => store;
+export default withRedux(makeStore)(MyApp)
