1c3efe69e8118487a4df67be8693ac1cd6f0cdc9
amir-malek <ahmalekpour1999@gmail.com>
2021-12-04 13:02:01 +0330

    home Page desktop finished

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,20 +1,21 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, {Html, Head, Main, NextScript} from 'next/document'
 
 class MyDocument extends Document {
     static async getInitialProps(ctx) {
         const initialProps = await Document.getInitialProps(ctx)
-        return { ...initialProps }
+        return {...initialProps}
     }
 
     render() {
         return (
             <Html>
                 <Head>
-                    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet"/>
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;500&display=swap"
+                          rel="stylesheet"/>
                 </Head>
                 <body>
-                <Main />
-                <NextScript />
+                <Main/>
+                <NextScript/>
                 </body>
             </Html>
         )
