70a21094ad8af5ef5f23af035dfe23f62afb2878
Soorena ghasemizadeh <soorena.g2001@gmail.com>
2021-12-04 12:09:23 +0330

    half done for before demo

---
+++ components/Footer.js
@@ -0,0 +1,61 @@
+import classes from '../styles/Footer.module.scss'
+import {Button} from "@mui/material";
+import Head from "next/head";
+
+
+export default function Footer() {
+
+    return <>
+        <Head>
+            <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet"/>
+        </Head>
+        <div className={classes.footerMain}>
+            <div className={classes.registerText}>
+                Register to our newsletter
+            </div>
+            <div className={classes.emailSec}>
+                <input className={classes.emailInput} type="text" placeholder='Email Address'/>
+                <Button className={classes.registerBtn} >Register</Button>
+            </div>
+            <div className={classes.dividingLine}>
+            </div>
+            <div className={classes.linksMainSec}>
+                <div className={classes.logoSec}>
+                    LOGO
+                </div>
+                <div className={classes.linksSec}>
+                    <div className={classes.linkTitle}>
+                        Support
+                    </div>
+                    <div className={classes.linkSubtitle}>
+                        Contact Us
+                    </div>
+                    <div className={classes.linkSubtitle}>
+                        About Us
+                    </div>
+                    <div className={classes.linkSubtitle}>
+                        FAQ
+                    </div>
+                </div>
+                <div className={classes.linksSec}>
+                    <div className={classes.linkTitle}>
+                        Legal
+                    </div>
+                    <div className={classes.linkSubtitle}>
+                        Terms of Service
+                    </div>
+                    <div className={classes.linkSubtitle}>
+                        Privacy Policy
+                    </div>
+                </div>
+            </div>
+            <div className={classes.socialMediaSec}>
+                <img src="/icons/instagram.svg" alt=""/>
+                <img style={{marginLeft:32,}} src="/icons/linkedIn.svg" alt=""/>
+                <img style={{marginLeft:32,}} src="/icons/telegram.svg" alt=""/>
+                <img style={{marginLeft:32,}} src="/icons/twitter.svg" alt=""/>
+
+            </div>
+        </div>
+    </>
+}
---
+++ components/HomePage.js
@@ -0,0 +1,105 @@
+import classes from '../styles/HomePage.module.scss'
+import {Button} from "@mui/material";
+import Head from "next/head";
+
+
+export default function HomePage() {
+
+    return <>
+        <Head>
+            <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet"/>
+        </Head>
+        <div className={classes.main}>
+            <div className={classes.topMainSec}>
+                <div className={classes.topLeftSec}>
+                    <div className={classes.artWorkName}>
+                        Starry Night
+                    </div>
+                    <div className={classes.artistName}>
+                        By Vincent Van Gogh
+                    </div>
+                    <div className={classes.artworkInfo}>
+                        is an oil-on-canvas painting by the Dutch Post-Impressionist painter Vincent van Gogh. Painted
+                        in June 1889, it depicts the view from the east-facing window of his asylum room at
+                        Saint-Rémy-de-Provence, just before sunrise, with the addition of an imaginary village
+                    </div>
+                    <Button className={classes.buyBtn}>Buy Now</Button>
+                </div>
+                <div className={classes.topRightSec}>
+                    <div>
+                        <img className={classes.artWorkImg} src="/images/starryNight.png" alt=""/>
+                    </div>
+                    <div className={classes.artWorkDetailSec}>
+                        <div className={classes.priceMainSec}>
+                            <div className={classes.priceSec}>
+                                <div className={classes.titleSec}>
+                                    Price
+                                </div>
+                                <div className={classes.amountSec}>
+                                    765 ETH
+                                </div>
+                            </div>
+                            <div className={classes.quantSec}>
+                                <div className={classes.title2Sec}>
+                                    Quantity
+                                </div>
+                                <div className={classes.amountSec}>
+                                    12 Token
+                                </div>
+                            </div>
+                        </div>
+                        <div className={classes.dayHourMinSec}>
+                            <div className={classes.Sec}>
+                                <div className={classes.Num}>
+                                    02
+                                </div>
+                                <div className={classes.Txt}>
+                                    Day
+                                </div>
+                            </div>
+                            <div className={classes.Sec}>
+                                <div className={classes.Num}>
+                                    18
+                                </div>
+                                <div className={classes.Txt}>
+                                    Hour
+                                </div>
+                            </div>
+                            <div className={classes.Sec}>
+                                <div className={classes.Num}>
+                                    40
+                                </div>
+                                <div className={classes.Txt}>
+                                    Min
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={classes.galleryMainSec}>
+                <div className={classes.galleryTopSec}>
+                    <div className={classes.galleryDescMainSec}>
+                        <div className={classes.galleryName}>
+                            DD Gallery
+                        </div>
+                        <div className={classes.galleryDescSec}>
+                            <img className={classes.imgSec} src="/images/img.png" alt=""/>
+                            <div className={classes.galleryDesc}>
+                                <div className={classes.galleryDecsTxt}>
+                                    A non-fungible token is a unique and non-interchangeable unit of data stored on a
+                                    digital ledger. NFTs can be associated with easily-reproducible
+                                    <br/>items such
+                                </div>
+                                <Button className={classes.exploreBtn} >Explore</Button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={classes.galleryMainPhoto}>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+}
