df24059acd572f44e86265cacc5f039226edc612
Soorena ghasemizadeh <soorena.g2001@gmail.com>
2021-12-07 12:14:41 +0330

    modal push haminjoory

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,12 @@ import classes from '../styles/Header.module.scss'
 import {Button, useMediaQuery} from "@mui/material";
 import {ethers} from "ethers"
 import { useTheme } from '@mui/material/styles';
+import * as React from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Typography from '@mui/material/Typography';
 
 export default function Header() {
 
@@ -13,12 +19,29 @@ export default function Header() {
     const theme = useTheme();
     const matches = useMediaQuery(theme.breakpoints.down('sm'));
 
+    const style = {
+        position: 'absolute',
+        outline: 0,
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 550,
+        bgcolor: 'background.paper',
+        // border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+    };
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return <>
         <div className={classes.mainHeader}>
             {matches ?
                 <>
                     <div className={classes.headerMainMob}>
-                        <Button className={classes.registerBtnMob}>Connect Wallet</Button>
+                        <Button className={classes.registerBtnMob} onClick={handleOpen} >Connect Wallet</Button>
                         <div className={classes.hamburgerAndlogo}>
                             <div className={classes.logoMob}>
 
@@ -35,13 +58,34 @@ export default function Header() {
                         <li>Art Centers</li>
                         <li>Artists</li>
                         <li>About NFT</li>
-                        <Button onClick={connectWallet} className={classes.registerBtn} disableElevation
+                        <Button onClick={handleOpen} className={classes.registerBtn} disableElevation
                                 variant={"contained"}>
                             Connect Wallet
                         </Button>
                     </ul>
                 </>
             }
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={open}>
+                    <Box sx={style}>
+                     <div className={classes.modalMainSec}>
+                        <div style={{fontSize:20}} className={classes.connectWalletTitle}>
+                            Connect your wallet
+                        </div>
+                     </div>
+                    </Box>
+                </Fade>
+            </Modal>
         </div>
     </>
 }
