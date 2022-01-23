17cf87e409942a7992b979d4e1fbae1d6956fa50
Saeed <saeedhst@gmail.com>
2022-01-23 13:44:06 +0330

    artist List

---
+++ pages/artist-list/index.js
@@ -0,0 +1,104 @@
+import classes from '../../styles/ArtistList/artistList.module.scss'
+import dynamic from "next/dynamic";
+import {Slide} from "react-slideshow-image";
+import 'react-slideshow-image/dist/styles.css';
+import {useTheme} from "@mui/material/styles";
+import {useMediaQuery} from "@mui/material";
+import {useEffect, useRef} from "react";
+
+export default function artistList() {
+    const galleryImages = [
+        "images/img_7.png",
+        "images/img_7.png",
+        "images/img_7.png",
+        "images/img_7.png",
+        "images/img_7.png",
+        "images/img_7.png",
+    ]
+
+    const Map = dynamic(() => {
+        console.log('hi')
+        return import('../../components/ArtCenters/Map')
+    }, {ssr: false})
+    const theme = useTheme();
+    const matches = useMediaQuery(theme.breakpoints.down('sm'));
+    const gallerySliderRef = useRef()
+    const relatedSliderRef = useRef()
+
+    useEffect(() => {
+        let marker = document.getElementsByClassName('leaflet-marker-icon')[0];
+        console.log(marker)
+        // marker.src = marker.src.slice(0, 65)
+    }, [])
+
+
+    return (
+        <>
+            <div className={classes.sortSec}>
+                <div className={classes.sort}> Sort by</div>
+                <div className={classes.sortList}>
+                    <div className={classes.sortListText}>Newest</div>
+                </div>
+            </div>
+
+            <div className={classes.root}>
+                <div className={classes.row}>
+                    <div className={classes.item}>
+                        <div className={classes.img} style={{backgroundImage: `url("images/img_8.png")`}}></div>
+                        <div className={classes.imgTitle}>Reza Derakhshani</div>
+                    </div>
+                    <div className={classes.item}>
+                        <div className={classes.img} style={{backgroundImage: `url("images/img_8.png")`}}></div>
+                        <div className={classes.imgTitle}>Reza Derakhshani</div>
+                    </div>
+                    <div className={classes.item}>
+                        <div className={classes.img} style={{backgroundImage: `url("images/img_8.png")`}}></div>
+                        <div className={classes.imgTitle}>Reza Derakhshani</div>
+                    </div>
+                </div>
+                <div className={classes.row}>
+                    <div className={classes.item}>
+                        <div className={classes.img}></div>
+                        <div className={classes.imgTitle}>Reza Derakhshani</div>
+                    </div>
+                    <div className={classes.item}>
+                        <div className={classes.img}></div>
+                        <div className={classes.imgTitle}>Reza Derakhshani</div>
+                    </div>
+                    <div className={classes.item}>
+                        <div className={classes.img}></div>
+                        <div className={classes.imgTitle}>Reza Derakhshani</div>
+                    </div>
+                </div>
+                <div className={classes.row}>
+                    <div className={classes.item}>
+                        <div className={classes.img}></div>
+                        <div className={classes.imgTitle}>Reza Derakhshani</div>
+                    </div>
+                    <div className={classes.item}>
+                        <div className={classes.img}></div>
+                        <div className={classes.imgTitle}>Reza Derakhshani</div>
+                    </div>
+                    <div className={classes.item}>
+                        <div className={classes.img}></div>
+                        <div className={classes.imgTitle}>Reza Derakhshani</div>
+                    </div>
+                </div>
+                <div className={classes.row}>
+                    <div className={classes.item}>
+                        <div className={classes.img}></div>
+                        <div className={classes.imgTitle}>Reza Derakhshani</div>
+                    </div>
+                    <div className={classes.item}>
+                        <div className={classes.img}></div>
+                        <div className={classes.imgTitle}>Reza Derakhshani</div>
+                    </div>
+                    <div className={classes.item}>
+                        <div className={classes.img}></div>
+                        <div className={classes.imgTitle}>Reza Derakhshani</div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
