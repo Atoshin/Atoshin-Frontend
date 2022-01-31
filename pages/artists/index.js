77a6b1e60c8d2a215938f32b2b36b9e47d7757cb
amir-malek <ahmalekpour1999@gmail.com>
2022-01-31 10:59:29 +0330

    artists ssr enabled

--- pages/artists/index.js
+++ pages/artists/index.js
@@ -1,24 +1,30 @@
 import classes from '../../styles/ArtistList/artistList.module.scss'
 import {useTheme} from "@mui/material/styles";
 import axios from "axios";
+import Link from 'next/link';
 
 export default function Artists({artists}) {
-
     const theme = useTheme();
 
     return (
         <>
             <div className={classes.ArtistTitle}>
-                Artist
+                Artists
             </div>
             <div className={classes.root}>
                 <div className={classes.row}>
                     {artists.map(artist => {
-                        return <div className={classes.item}>
-                            <div className={classes.img}
-                                 style={{backgroundImage: `url("${artist.media.find(media => media.main === 1).url}")`}}/>
-                            <div className={classes.imgTitle}>{artist.fullName}</div>
-                        </div>
+                        return <Link href={`/artists/${artist.fullName.toLowerCase().replace(/ /g, "-")}/${artist.id}`}>
+                            <div className={classes.item}>
+                                <div className={classes.img}
+                                     style={{
+                                         backgroundImage: `url("${artist.medias.find(media => media.main === 1).url}")`,
+                                         backgroundSize: "cover",
+                                         backgroundPosition: "center"
+                                     }}/>
+                                <div className={classes.imgTitle}>{artist.fullName}</div>
+                            </div>
+                        </Link>
                     })}
                 </div>
             </div>
