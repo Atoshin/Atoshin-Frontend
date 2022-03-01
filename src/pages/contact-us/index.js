d13cdd77b73670f3fbc7849c2a3d4d98b1fb045b
atiye <atiyenaderi81@gmail.com>
2022-03-01 16:55:16 +0330

    ContactUs Ui Changes

--- src/pages/contact-us/index.js
+++ src/pages/contact-us/index.js
@@ -1,6 +1,8 @@
 import classes from '../../styles/ContactUs/contactUs.module.scss'
 import {useTheme} from "@mui/material/styles";
 import {useMediaQuery} from "@mui/material";
+import Button from '@mui/material/Button';
+
 import {useEffect} from "react";
 
 export default function ContactUs({galleries}) {
@@ -27,34 +29,48 @@ export default function ContactUs({galleries}) {
                 >
                     <div className={classes.contactUs}>CONTACT US</div>
                     <div className={classes.howCan}>How can we help you?</div>
-                    <div className={classes.fill}>Fill in the form or drop us an email </div>
+                    <div className={classes.fill}>Fill in the form or drop an email for us </div>
                     <div className={classes.box1}>
                         <div className={classes.sec1}>
-                            <div className={classes.email}>Email</div>
+                            <div className={classes.email}>have you any question or problems?</div>
                             <div className={classes.emailC}>contact@atoshin.com</div>
                         </div>
                         <div className={classes.sec2}>
-                            <img src="/icons/email-icon.svg" alt=""/>
+                            <img src="/icons/email.svg" alt=""/>
+                        </div>
+                    </div>
+
+                    <div className={classes.box1}>
+                        <div className={classes.sec1}>
+                            <div className={classes.email}>want to show your museum or gallery in Atoshin?</div>
+                            <div className={classes.emailC}>joinus@atoshin.art</div>
+                        </div>
+                        <div className={classes.sec2}>
+                            <img src="/icons/email.svg" alt=""/>
                         </div>
                     </div>
                 </div>
                 <div>
                     <div className={classes.tableBox}>
-                        <div className={classes.name1}>
-                            <div className={classes.innerBox}>Name</div>
-                        </div>
-                        <div className={classes.name}>
-                            <div className={classes.innerBox}>Email</div>
-                        </div>
-                        <div className={classes.name}>
-                            <div className={classes.innerBox}>Subject</div>
-                        </div>
-                        <div className={classes.name} style={{height:124, display: 'block'}}>
-                            <div className={classes.innerBox} >Message</div>
-                        </div>
-                        <div className={classes.submit}>
-                            Submit
-                        </div>
+                        {/*<div className={classes.name1}>*/}
+                        {/*    <div className={classes.innerBox}>Name</div>*/}
+                        {/*</div>*/}
+                        <input type="text" className={classes.name1} placeholder='Name'/>
+                        {/*<div className={classes.name}>*/}
+                        {/*    <div className={classes.innerBox}>Email</div>*/}
+                        {/*</div>*/}
+                        <input type="text" className={classes.name} placeholder='Email'/>
+
+                        <input type="text" className={classes.name} placeholder='Subject'/>
+
+                        {/*<div className={classes.name} style={{height:124, display: 'block'}}>*/}
+                        {/*    <div className={classes.innerBox} >Message</div>*/}
+                        {/*</div>*/}
+                        <input type="text" style={{height:124, display: 'block'}} className={classes.name} placeholder='Message'/>
+
+                        <Button variant="contained" className={classes.submit}>
+                            <span style={{textTransform: 'upperCase'}}>S</span>ubmit
+                        </Button>
                     </div>
 
                 </div>
