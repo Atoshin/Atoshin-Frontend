4eef16aeff07c07d423166785ef57dba310ead47
Saeed <saeedhst@gmail.com>
2022-03-09 14:57:14 +0330

    main page two bottom sliders mobile ui

--- src/components/HomePage/GallerySection/GallerySlider.js
+++ src/components/HomePage/GallerySection/GallerySlider.js
@@ -1,35 +1,28 @@
-import classes from "../../styles/ArtistsSlider/ArtistsSlider.module.scss";
+import classes from "../../../styles/GallerySlider/GallerySlider.module.scss";
 import {Slide} from "react-slideshow-image";
+
 import 'react-slideshow-image/dist/styles.css'
 import {useTheme} from "@mui/material/styles";
 import {useMediaQuery} from "@mui/material";
-import {useRef} from "react";
+import {useRef, useState} from "react";
 import {useRouter} from "next/router";
 import Link from "next/link";
 import * as React from "react";
 
-export default function ArtistsSlider({artists}) {
+export default function ArtistsSlider({artists, setSelectedGallery}) {
     const theme = useTheme();
     const matches = useMediaQuery(theme.breakpoints.down('sm'));
     const sliderRef = useRef();
     const router = useRouter();
 
+
+
     return <div className={classes.artistMainSec}>
-        <div className={classes.artistTitle}>
-            Artists
-        </div>
-        <div className={classes.moreArtists}>
-            <p className={classes.artistsSubtext}>
-                Learn more about these great artists
-            </p>
-            <p onClick={() => router.push('/artists')} className={classes.viewAll}>
-                View All
-            </p>
-        </div>
+
+
         <div className={classes.artistsSlider}>
-            <img className={classes.vectorLeft} onClick={() => {
-                sliderRef.current.goBack()
-            }}
+            <img className={classes.vectorLeft}
+                 // onClick={() => {sliderRef.current.goBack()}}
                  src="/icons/vector-left.svg" alt=""/>
             <Slide ref={sliderRef} easing={"ease"} slidesToShow={matches ? 2 : 4} infinite={true} arrows={false}
                    slidesToScroll={1}
@@ -37,10 +30,10 @@ export default function ArtistsSlider({artists}) {
                    duration={5000}>
                 {artists.map((artist, idx) => {
                     return (
-                        <Link key={idx} href={`/artists/${artist.fullName.toLowerCase().replace(/ /g, '-')}/${artist.id}`}>
+                        // <Link key={idx} href={'/'}>
                             <a>
                                 {/*onClick={() => router.push(`/artists/${artist.fullName.toLowerCase().replace(/ /g, '-')}/${artist.id}`)}*/}
-                                <div key={artist.id} className={classes.artist}>
+                                <div onClick={() => setSelectedGallery(artist)} key={artist.id} className={classes.artist}>
                                     {/*<img src={artist.medias.find(media => media.main === 1).url} alt="" style={{borderRadius:" 3px 3px 0px 0px"}}/>*/}
                                     <div style={{
                                         backgroundImage: `url("${artist.medias.find(media => media.main === 1).url}")`,
@@ -51,16 +44,15 @@ export default function ArtistsSlider({artists}) {
                                         backgroundPosition: 'center'
                                     }}>
                                     </div>
-                                    <p>{artist.fullName}</p>
+                                    <p>{artist.name}</p>
                                 </div>
                             </a>
-                        </Link>
+                        // </Link>
                     )
                 })}
             </Slide>
-            <img className={classes.vectorRight} onClick={() => {
-                sliderRef.current.goNext()
-            }}
+            <img className={classes.vectorRight}
+                 // onClick={() => {sliderRef.current.goNext()}}
                  src="/icons/vector-right.svg" alt=""/>
         </div>
     </div>
