45a30d1c205ea6b3662804d3cb31f2210d4886bf
atiye <atiyenaderi81@gmail.com>
2022-04-26 15:38:00 +0430

    OwnersModal Ui fix

--- src/components/ShowAsset/OwnersModal.js
+++ src/components/ShowAsset/OwnersModal.js
@@ -14,44 +14,46 @@ export default function OwnersModal(props) {
     return (
         <Dialog onClose={handleClose} open={open}>
 
-            <div className={classes.dialogHeaderSticky}>
-                <div className={classes.dialogHeader}>
-                    <DialogTitle className={classes.modalTitle}>Other Owners</DialogTitle>
-                    <div className={classes.vectorX} onClick={handleClose}>
-                        <img src="/icons/vector-X.png" alt=""/>
-                    </div>
-                </div>
-                <div className={classes.ownersIndexTitlesSec}>
-                    <div className={classes.rankTitle}>
-                        Rank
-                    </div>
-                    <div className={classes.ownersTitle}>
-                        Owners
-                    </div>
-                    <div className={classes.quantityTitle}>
-                        Quantity
-                    </div>
-                </div>
-            </div>
-
-            <div className={classes.indexSec}>
-                {owners.map((owner, idx) => {
-                    return (
-                        <div key={idx} className={classes.ownersIndexRow}>
-                            <div className={classes.rankNum}>
-                                {idx + 1}
-                            </div>
-                            <a rel="noreferrer" target="_blank"
-                               href={process.env.NEXT_PUBLIC_ETHERSCAN_DOMAIN + 'address/' + owner.address}
-                               className={classes.ownerName}>
-                                {owner.address.slice(0, 4) + '...' + owner.address.slice(-4)}
-                            </a>
-                            <div className={classes.quantity}>
-                                {owner.tokens} Token{owner.tokens > 1 && 's'}
-                            </div>
+            <div className={classes.dialog}>
+                <div className={classes.dialogHeaderSticky}>
+                    <div className={classes.dialogHeader}>
+                        <DialogTitle className={classes.modalTitle}>Other Owners</DialogTitle>
+                        <div className={classes.vectorX} onClick={handleClose}>
+                            <img src="/icons/vector-X.png" alt=""/>
                         </div>
-                    )
-                })}
+                    </div>
+                    <div className={classes.ownersIndexTitlesSec}>
+                        <div className={classes.rankTitle}>
+                            Rank
+                        </div>
+                        <div className={classes.ownersTitle}>
+                            Owners
+                        </div>
+                        <div className={classes.quantityTitle}>
+                            Quantity
+                        </div>
+                    </div>
+                </div>
+
+                <div className={classes.indexSec}>
+                    {owners.map((owner, idx) => {
+                        return (
+                            <div key={idx} className={classes.ownersIndexRow}>
+                                <div className={classes.rankNum}>
+                                    {idx + 1}
+                                </div>
+                                <a rel="noreferrer" target="_blank"
+                                   href={process.env.NEXT_PUBLIC_ETHERSCAN_DOMAIN + 'address/' + owner.address}
+                                   className={classes.ownerName}>
+                                    {owner.address.slice(0, 4) + '...' + owner.address.slice(-4)}
+                                </a>
+                                <div className={classes.quantity}>
+                                    {owner.tokens} Token{owner.tokens > 1 && 's'}
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
             {/*<div className={classes.ownersModalIndexMain}>*/}
             {/*<div className={classes.indexSec}>*/}
