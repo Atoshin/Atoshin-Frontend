f09615635f769838f010a6467bc5e285a40416d1
amir-malek <ahmalekpour1999@gmail.com>
2022-06-01 18:28:31 +0430

    changes to login procedure

--- src/pages/sign-message/index.js
+++ src/pages/sign-message/index.js
@@ -2,9 +2,10 @@ import classes from '../../styles/SignMessage/SignMessage.module.scss';
 import {useEffect} from "react";
 import {useAppDispatch} from "../../redux/hooks";
 import {setOpen} from "../../redux/slices/connectWalletModalSlice";
+import {parseCookies} from "../../functions/parseCookies";
 
 
-export default function SignMessage() {
+export default function SignMessage({data}) {
     const dispatch = useAppDispatch()
 
 
@@ -15,4 +16,15 @@ export default function SignMessage() {
 
     return <div className={classes.root}>
     </div>
+}
+
+
+export async function getServerSideProps({req, res}) {
+    const data = parseCookies(req)
+
+    return {
+        props: {
+            data
+        }
+    }
 }
